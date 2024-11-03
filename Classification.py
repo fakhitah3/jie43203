2af86fecd508cb2d77e36f4744652700a7258519
fakhitah3 <fakhitah.ridzuan@gmail.com>
2024-11-03 12:45:07 +0800

    Update Classification.py

--- Classification.py
+++ Classification.py
@@ -1,1 +1,12 @@
+# Import necessary modules
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
 
+import streamlit as st
+
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv("https://raw.githubusercontent.com/fakhitah3/jie43203/refs/heads/main/Iris.csv")
+
+st.write(df)
