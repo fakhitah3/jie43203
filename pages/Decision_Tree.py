fea4355a9bdffcf3d677c0fe63d315e3114b515f
fakhitah3 <fakhitah.ridzuan@gmail.com>
2024-11-03 13:28:10 +0800

    Update Decision_Tree.py

--- pages/Decision_Tree.py
+++ pages/Decision_Tree.py
@@ -1,93 +1,72 @@
-# Importing the required packages
+import streamlit as st
+# Import necessary modules
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
 import numpy as np
 import pandas as pd
-from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.datasets import load_iris
 import matplotlib.pyplot as plt
 
+df = pd.read_csv("https://raw.githubusercontent.com/fakhitah3/jie43203/refs/heads/main/Iris.csv")
 
-# Function to import the dataset
-def importdata():
-	balance_data = pd.read_csv(
-		'https://archive.ics.uci.edu/ml/machine-learning-' +
-		'databases/balance-scale/balance-scale.data',
-		sep=',', header=None)
+st.write(df)
 
-	# Displaying dataset information
-	print("Dataset Length: ", len(balance_data))
-	print("Dataset Shape: ", balance_data.shape)
-	print("Dataset: ", balance_data.head())
-
-	return balance_data
-# Function to split the dataset into features and target variables
-def splitdataset(balance_data):
-
-	# Separating the target variable
-	X = balance_data.values[:, 1:5]
-	Y = balance_data.values[:, 0]
-
-	# Splitting the dataset into train and test
-	X_train, X_test, y_train, y_test = train_test_split(
-		X, Y, test_size=0.3, random_state=100)
-
-	return X, Y, X_train, X_test, y_train, y_test
-
-def train_using_gini(X_train, X_test, y_train):
-
-	# Creating the classifier object
-	clf_gini = DecisionTreeClassifier(criterion="gini", random_state=100, max_depth=3, min_samples_leaf=5)
-
-	# Performing training
-	clf_gini.fit(X_train, y_train)
-	return clf_gini
-  
-def train_using_entropy(X_train, X_test, y_train):
-
-	# Decision tree with entropy
-	clf_entropy = DecisionTreeClassifier(
-		criterion="entropy", random_state=100,
-		max_depth=3, min_samples_leaf=5)
-
-	# Performing training
-	clf_entropy.fit(X_train, y_train)
-	return clf_entropy
-
-# Function to make predictions
-def prediction(X_test, clf_object):
-	y_pred = clf_object.predict(X_test)
-	st.write("Predicted values:")
-	st.write(y_pred)
-	return y_pred
-
-# Placeholder function for cal_accuracy
-def cal_accuracy(y_test, y_pred):
-	st.write("Confusion Matrix: ",
-		confusion_matrix(y_test, y_pred))
-	st.write("Accuracy : ",
-		accuracy_score(y_test, y_pred)*100)
-	st.write("Report : ",
-		classification_report(y_test, y_pred))
-  
-if __name__ == "__main__":
-	data = importdata()
-	X, Y, X_train, X_test, y_train, y_test = splitdataset(data)
-
-	clf_gini = train_using_gini(X_train, X_test, y_train)
-	clf_entropy = train_using_entropy(X_train, X_test, y_train)
-
-from sklearn import tree
-
-# Putting the feature names and class names into variables
-fn = ['right-distance', 'right-weight', 'left-distance', 'left-weight']
-cn = ['L', 'B', 'R']
-
-tree.plot_tree(clf_gini,
-          feature_names=fn,
-          class_names=cn,
-          rounded=True, # Rounded node edges
-          filled=True, # Adds color according to class
-          proportion=True);
+# Loading data
+irisData = load_iris()
 
 
 
+# Create feature and target arrays
+X = irisData.data
+y = irisData.target
+
+# Split into training and test set
+X_train, X_test, y_train, y_test = train_test_split(
+			X, y, test_size = 0.2, random_state=42)
+
+
+X = df.iloc[:, 1:5].values
+y = df['Species'].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=42)
+
+knn3 = KNeighborsClassifier(n_neighbors=3)
+knn3.fit(X_train, y_train)
+
+# Predict on dataset which model has not seen before
+print(knn3.predict(X_test))
+
+knn5 = KNeighborsClassifier(n_neighbors=7)
+knn5.fit(X_train, y_train)
+
+# Predict on dataset which model has not seen before
+print(knn5.predict(X_test))
+
+# Calculate the accuracy of the model
+print("Accuracy n=3", knn3.score(X_test, y_test))
+print("Accuracy n=5", knn5.score(X_test, y_test))
+
+
+neighbors = np.arange(1, 9)
+train_accuracy = np.empty(len(neighbors))
+test_accuracy = np.empty(len(neighbors))
+
+# Loop over K values
+for i, k in enumerate(neighbors):
+	knn = KNeighborsClassifier(n_neighbors=k)
+	knn.fit(X_train, y_train)
+
+	# Compute training and test data accuracy
+	train_accuracy[i] = knn.score(X_train, y_train)
+	test_accuracy[i] = knn.score(X_test, y_test)
+
+# Generate plot
+plt.plot(neighbors, test_accuracy, label = 'Testing dataset Accuracy')
+plt.plot(neighbors, train_accuracy, label = 'Training dataset Accuracy')
+
+plt.legend()
+plt.xlabel('n_neighbors')
+plt.ylabel('Accuracy')
+plt.show()
+
+st.pyplot(plt.gcf())
