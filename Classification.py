b3464f67527c0fbeadae870fcfa44f370067909f
fakhitah3 <fakhitah.ridzuan@gmail.com>
2024-11-03 13:04:58 +0800

    Update Classification.py

--- Classification.py
+++ Classification.py
@@ -1,5 +1,7 @@
 import streamlit as st
-
+# Import necessary modules
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
 import numpy as np
 import pandas as pd
 
